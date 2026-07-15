816ad31fd6aa5cddbc86bba6ee50fbc3e1792594
inojBhashitha <main.inoj@gmail.com>
2026-07-15 15:42:32 +0530

    Setup JavaFX application with FXML

---
+++ src/main/java/com/relieftrack/app/Main.java
@@ -0,0 +1,28 @@
+package com.relieftrack.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                Main.class.getResource("/fxml/login.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("ReliefTrack");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
